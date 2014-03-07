bbd352f6939e9fce918d70faa5246dd73a946704
Ric Diesel <ric.diesel@gmail.com>
2014-03-07 14:42:11 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RadDemoApp::Application.config.secret_key_base = '6c03f9fcddea3c53febcb90ce694bd702ad6262a231183838210cd680837618bb167d422c8029fbba30d43c1b91b764450fff3641f1504eb9c19c5cbd44ae11f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RadDemoApp::Application.config.session_store :cookie_store, key: '_rad_demo_app_session'
